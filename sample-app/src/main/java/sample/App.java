9d3d249c1ccf0fd8f692f8b15c4791f76f7300da
Mark Phelps <209477+markphelps@users.noreply.github.com>
2022-11-26 14:57:34 -0500

    chore: try to fix java build

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -4,17 +4,21 @@ import com.flipt.api.client.flags.exceptions.GetException;
 import com.flipt.api.client.flags.types.FliptFlag;
 import com.flipt.api.core.BearerAuth;
 
-String token = System.getenv("FLIPT_API_TOKEN");
+public class App {
+  public static void main(String[] args) {
+    String token = System.getenv("FLIPT_API_TOKEN");
 
-BearerAuth auth = BearerAuth.of(token);
+    BearerAuth auth = BearerAuth.of(token);
 
-FliptApiClient fliptApiClient = new FliptApiClient("http://localhost:8080", auth);
+    FliptApiClient fliptApiClient = new FliptApiClient("http://localhost:8080", auth);
 
-try {
-    FliptFlag fliptFlag = fliptApiClient.flags().get(Get.Request.builder()
-            .key("flag_abc123")
-            .build());
-    System.out.println("Successfully fetched flag with id" + fliptFlag.getKey());
-} catch (GetException e) {
-    System.out.println("Encountered error while getting flag" + e.getMessage());
+    try {
+        FliptFlag fliptFlag = fliptApiClient.flags().get(Get.Request.builder()
+                .key("flag_abc123")
+                .build());
+        System.out.println("Successfully fetched flag with id" + fliptFlag.getKey());
+    } catch (GetException e) {
+        System.out.println("Encountered error while getting flag" + e.getMessage());
+    }
+  }
 }
