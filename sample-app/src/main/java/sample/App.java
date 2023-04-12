e280d90e4fd311fbc6e53a6945ffd8fef52a5d27
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-04-12 09:50:07 -0400

    chore: fix java sample

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -4,13 +4,16 @@ import com.flipt.api.FliptApiClient;
 import com.flipt.api.resources.flags.types.Flag;
 
 public class App {
+  private static final String DEFAULT_NAMESPACE = "default";
+  private static final String FLIPT_URL = "http://localhost:8080";
+
   public static void main(String[] args) {
     String token = System.getenv("FLIPT_API_TOKEN");
 
-    FliptApiClient fliptApiClient = FliptApiClient.builder().token(token).url("http://localhost:8080").build();
+    FliptApiClient fliptApiClient = FliptApiClient.builder().token(token).url(FLIPT_URL).build();
 
     try {
-      Flag flag = fliptApiClient.flags().get("flag_abc123");
+      Flag flag = fliptApiClient.flags().get(DEFAULT_NAMESPACE, "flag_abc123");
       System.out.println("Successfully fetched flag with id" + flag.getKey());
     } catch (Exception e) {
       System.out.println("Encountered error while getting flag" + e.getMessage());
