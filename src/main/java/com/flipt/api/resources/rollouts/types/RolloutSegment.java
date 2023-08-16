ab15636cc57355a4f9b8aeca9c43d3233178e826
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-16 14:36:55 +0000

    SDK regeneration

--- src/main/java/com/flipt/api/resources/rollouts/types/RolloutSegment.java
+++ src/main/java/com/flipt/api/resources/rollouts/types/RolloutSegment.java
@@ -4,24 +4,47 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JsonDeserialize(builder = RolloutSegment.Builder.class)
 public final class RolloutSegment {
     private final String segmentKey;
 
+    private final Optional<List<String>> segmentKeys;
+
+    private final Optional<RolloutSegmentOperator> segmentOperator;
+
     private final boolean value;
 
-    private RolloutSegment(String segmentKey, boolean value) {
+    private RolloutSegment(
+            String segmentKey,
+            Optional<List<String>> segmentKeys,
+            Optional<RolloutSegmentOperator> segmentOperator,
+            boolean value) {
         this.segmentKey = segmentKey;
+        this.segmentKeys = segmentKeys;
+        this.segmentOperator = segmentOperator;
         this.value = value;
     }
 
     @JsonProperty("segmentKey")
     public String getSegmentKey() {
         return segmentKey;
+    }
+
+    @JsonProperty("segmentKeys")
+    public Optional<List<String>> getSegmentKeys() {
+        return segmentKeys;
+    }
+
+    @JsonProperty("segmentOperator")
+    public Optional<RolloutSegmentOperator> getSegmentOperator() {
+        return segmentOperator;
     }
 
     @JsonProperty("value")
@@ -36,17 +59,21 @@ public final class RolloutSegment {
     }
 
     private boolean equalTo(RolloutSegment other) {
-        return segmentKey.equals(other.segmentKey) && value == other.value;
+        return segmentKey.equals(other.segmentKey)
+                && segmentKeys.equals(other.segmentKeys)
+                && segmentOperator.equals(other.segmentOperator)
+                && value == other.value;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.segmentKey, this.value);
+        return Objects.hash(this.segmentKey, this.segmentKeys, this.segmentOperator, this.value);
     }
 
     @Override
     public String toString() {
-        return "RolloutSegment{" + "segmentKey: " + segmentKey + ", value: " + value + "}";
+        return "RolloutSegment{" + "segmentKey: " + segmentKey + ", segmentKeys: " + segmentKeys + ", segmentOperator: "
+                + segmentOperator + ", value: " + value + "}";
     }
 
     public static SegmentKeyStage builder() {
@@ -65,6 +92,14 @@ public final class RolloutSegment {
 
     public interface _FinalStage {
         RolloutSegment build();
+
+        _FinalStage segmentKeys(Optional<List<String>> segmentKeys);
+
+        _FinalStage segmentKeys(List<String> segmentKeys);
+
+        _FinalStage segmentOperator(Optional<RolloutSegmentOperator> segmentOperator);
+
+        _FinalStage segmentOperator(RolloutSegmentOperator segmentOperator);
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -73,11 +108,17 @@ public final class RolloutSegment {
 
         private boolean value;
 
+        private Optional<RolloutSegmentOperator> segmentOperator = Optional.empty();
+
+        private Optional<List<String>> segmentKeys = Optional.empty();
+
         private Builder() {}
 
         @Override
         public Builder from(RolloutSegment other) {
             segmentKey(other.getSegmentKey());
+            segmentKeys(other.getSegmentKeys());
+            segmentOperator(other.getSegmentOperator());
             value(other.getValue());
             return this;
         }
@@ -97,8 +138,34 @@ public final class RolloutSegment {
         }
 
         @Override
+        public _FinalStage segmentOperator(RolloutSegmentOperator segmentOperator) {
+            this.segmentOperator = Optional.of(segmentOperator);
+            return this;
+        }
+
+        @Override
+        @JsonSetter(value = "segmentOperator", nulls = Nulls.SKIP)
+        public _FinalStage segmentOperator(Optional<RolloutSegmentOperator> segmentOperator) {
+            this.segmentOperator = segmentOperator;
+            return this;
+        }
+
+        @Override
+        public _FinalStage segmentKeys(List<String> segmentKeys) {
+            this.segmentKeys = Optional.of(segmentKeys);
+            return this;
+        }
+
+        @Override
+        @JsonSetter(value = "segmentKeys", nulls = Nulls.SKIP)
+        public _FinalStage segmentKeys(Optional<List<String>> segmentKeys) {
+            this.segmentKeys = segmentKeys;
+            return this;
+        }
+
+        @Override
         public RolloutSegment build() {
-            return new RolloutSegment(segmentKey, value);
+            return new RolloutSegment(segmentKey, segmentKeys, segmentOperator, value);
         }
     }
 }
--- src/main/java/com/flipt/api/resources/rules/types/RuleCreateRequest.java
+++ src/main/java/com/flipt/api/resources/rules/types/RuleCreateRequest.java
@@ -4,24 +4,47 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JsonDeserialize(builder = RuleCreateRequest.Builder.class)
 public final class RuleCreateRequest {
     private final String segmentKey;
 
+    private final Optional<List<String>> segmentKeys;
+
+    private final Optional<RuleSegmentOperator> segmentOperator;
+
     private final int rank;
 
-    private RuleCreateRequest(String segmentKey, int rank) {
+    private RuleCreateRequest(
+            String segmentKey,
+            Optional<List<String>> segmentKeys,
+            Optional<RuleSegmentOperator> segmentOperator,
+            int rank) {
         this.segmentKey = segmentKey;
+        this.segmentKeys = segmentKeys;
+        this.segmentOperator = segmentOperator;
         this.rank = rank;
     }
 
     @JsonProperty("segmentKey")
     public String getSegmentKey() {
         return segmentKey;
+    }
+
+    @JsonProperty("segmentKeys")
+    public Optional<List<String>> getSegmentKeys() {
+        return segmentKeys;
+    }
+
+    @JsonProperty("segmentOperator")
+    public Optional<RuleSegmentOperator> getSegmentOperator() {
+        return segmentOperator;
     }
 
     @JsonProperty("rank")
@@ -36,17 +59,21 @@ public final class RuleCreateRequest {
     }
 
     private boolean equalTo(RuleCreateRequest other) {
-        return segmentKey.equals(other.segmentKey) && rank == other.rank;
+        return segmentKey.equals(other.segmentKey)
+                && segmentKeys.equals(other.segmentKeys)
+                && segmentOperator.equals(other.segmentOperator)
+                && rank == other.rank;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.segmentKey, this.rank);
+        return Objects.hash(this.segmentKey, this.segmentKeys, this.segmentOperator, this.rank);
     }
 
     @Override
     public String toString() {
-        return "RuleCreateRequest{" + "segmentKey: " + segmentKey + ", rank: " + rank + "}";
+        return "RuleCreateRequest{" + "segmentKey: " + segmentKey + ", segmentKeys: " + segmentKeys
+                + ", segmentOperator: " + segmentOperator + ", rank: " + rank + "}";
     }
 
     public static SegmentKeyStage builder() {
@@ -65,6 +92,14 @@ public final class RuleCreateRequest {
 
     public interface _FinalStage {
         RuleCreateRequest build();
+
+        _FinalStage segmentKeys(Optional<List<String>> segmentKeys);
+
+        _FinalStage segmentKeys(List<String> segmentKeys);
+
+        _FinalStage segmentOperator(Optional<RuleSegmentOperator> segmentOperator);
+
+        _FinalStage segmentOperator(RuleSegmentOperator segmentOperator);
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -73,11 +108,17 @@ public final class RuleCreateRequest {
 
         private int rank;
 
+        private Optional<RuleSegmentOperator> segmentOperator = Optional.empty();
+
+        private Optional<List<String>> segmentKeys = Optional.empty();
+
         private Builder() {}
 
         @Override
         public Builder from(RuleCreateRequest other) {
             segmentKey(other.getSegmentKey());
+            segmentKeys(other.getSegmentKeys());
+            segmentOperator(other.getSegmentOperator());
             rank(other.getRank());
             return this;
         }
@@ -97,8 +138,34 @@ public final class RuleCreateRequest {
         }
 
         @Override
+        public _FinalStage segmentOperator(RuleSegmentOperator segmentOperator) {
+            this.segmentOperator = Optional.of(segmentOperator);
+            return this;
+        }
+
+        @Override
+        @JsonSetter(value = "segmentOperator", nulls = Nulls.SKIP)
+        public _FinalStage segmentOperator(Optional<RuleSegmentOperator> segmentOperator) {
+            this.segmentOperator = segmentOperator;
+            return this;
+        }
+
+        @Override
+        public _FinalStage segmentKeys(List<String> segmentKeys) {
+            this.segmentKeys = Optional.of(segmentKeys);
+            return this;
+        }
+
+        @Override
+        @JsonSetter(value = "segmentKeys", nulls = Nulls.SKIP)
+        public _FinalStage segmentKeys(Optional<List<String>> segmentKeys) {
+            this.segmentKeys = segmentKeys;
+            return this;
+        }
+
+        @Override
         public RuleCreateRequest build() {
-            return new RuleCreateRequest(segmentKey, rank);
+            return new RuleCreateRequest(segmentKey, segmentKeys, segmentOperator, rank);
         }
     }
 }
