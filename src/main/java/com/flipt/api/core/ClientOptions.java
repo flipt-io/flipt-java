3a9c6e5512185b74c8a667ed74a3f0556c7c60ff
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-10-08 10:20:13 -0400

    chore: rm headers

--- src/main/java/com/flipt/api/core/ClientOptions.java
+++ src/main/java/com/flipt/api/core/ClientOptions.java
@@ -24,16 +24,8 @@ public final class ClientOptions {
         this.environment = environment;
         this.headers = new HashMap<>();
         this.headers.putAll(headers);
-        this.headers.putAll(Map.of(
-                "X-Fern-SDK-Name",
-                "com.flipt.fern:api-sdk",
-                "X-Fern-SDK-Version",
-                "0.2.12",
-                "X-Fern-Language",
-                "JAVA"));
         this.headerSuppliers = headerSuppliers;
         this.httpClient = httpClient;
-        ;
     }
 
     public Environment environment() {
--- src/test/java/com/flipt/api/TestClient.java
+++ src/test/java/com/flipt/api/TestClient.java
@@ -4,7 +4,6 @@ package com.flipt.api;
 
 public final class TestClient {
     public void test() {
-        // Add tests here and mark this file in .fernignore
         assert true;
     }
 }
